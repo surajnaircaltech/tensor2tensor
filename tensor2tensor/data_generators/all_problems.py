8389506ff6ef3353ec3413e52e3f6e70416bbefe
Suraj Nair <surajnair.caltech@gmail.com>
2021-09-01 15:22:00 -0700

    Adding new problems for LORL

--- tensor2tensor/data_generators/all_problems.py
+++ tensor2tensor/data_generators/all_problems.py
@@ -29,6 +29,7 @@ MODULES = [
     "tensor2tensor.data_generators.algorithmic_math_two_variables",
     "tensor2tensor.data_generators.allen_brain",
     "tensor2tensor.data_generators.audio",
+    "tensor2tensor.data_generators.lang_robot",
     "tensor2tensor.data_generators.batch_exploration_block0_max",
     "tensor2tensor.data_generators.batch_exploration_block0_smm",
     "tensor2tensor.data_generators.batch_exploration_block0_modelvar",
